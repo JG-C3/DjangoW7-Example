2fcee1914eab0c350bc7ca5b1fd1d1c977c9f730
JG-C3 <jgc3django@gmail.com>
2022-10-25 01:17:59 +0900

    Update Django W7

--- game/models.py
+++ game/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-# [미션] Weapon 모델을 Class로 생성.
-# [미션] models.Model 클래스를 상속받음
-# [미션] name : 문자열 필드, 최대 길이 20
-# [미션] power : 정수형 필드
 class Weapon(models.Model):
     name = models.CharField(max_length=20)
     power = models.IntegerField()
